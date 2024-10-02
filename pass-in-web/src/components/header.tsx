33d6fe2e9cd9ee2490af96c5a4d8f01ba7bc693d
tiago-soft-dev <tiago.soft.dev@gmail.com>
2024-10-02 19:42:20 -0300

    Update header.tsx

--- pass-in-web/src/components/header.tsx
+++ pass-in-web/src/components/header.tsx
@@ -13,25 +13,3 @@ export function Header(){
     )
 }
 
-export function Header(){
-    return (
-        <div className='flex items-center gap-5 py-2'>
-            <img src={logo} alt="logo"  />
-            <nav className='flex items-center gap-5'>
-                <a href='' className='font-medium text-sm text-zinc-300'>Eventos</a>
-                <a href='' className='font-medium text-sm'>Participações</a>
-                
-            </nav>
-        </div>
-=======
-import nlwUniteIcon from '../assets/nlw-unite-icon.svg'
-
-export function Header(){
-    return (
-       <div>
-        <img src={nlwUniteIcon} alt="logo"/>
-
-       </div>
->>>>>>> 641d44b8a37688866710ea0a7f08fa3a766c4dc3
-    )
-}
